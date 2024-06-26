dd24df9c49b5fb7c5cacd491b7bca294bd3f049b
matheus-nantes <matheus.nantes246@gmail.com>
2024-06-26 05:30:59 -0400

    implmentação de rotação e inicío da implementação da lógica da nomenclatura

--- teste.py
+++ teste.py
@@ -6,54 +6,46 @@ from ultralytics import YOLO
 import os
 import shutil
 from screeninfo import get_monitors
-from tkinter import Tk, filedialog  # Importa o módulo filedialog do tkinter
-from index_calculation import IndexCalculation 
-
+from tkinter import Tk, filedialog, simpledialog
+from index_calculation import IndexCalculation
 
 
 def select_file():
     root = Tk()
-    root.withdraw()  # Esconde a janela principal do tkinter
-
-    # Abre a janela de seleção de arquivo
+    root.withdraw()
     file_path = filedialog.askopenfilename(filetypes=[("TIFF files", "*.tif"), ("All files", "*.*")])
-
     if not file_path:
         print("Nenhum arquivo selecionado. Encerrando o programa.")
         sys.exit(0)
-
     return file_path
 
-# Caminho do ortomosaico TIFF
-#file_path = "M:\\EMBRAPA\\ORTOMOSAICOS\\SEDEB3B4_0-5CM_19-03-12_1-4\\3_dsm_ortho\\2_mosaic\\SEDEB3B4_0-5CM_19-03-12_1-4_transparent_mosaic_group1.tif"
-#file_path = "M:\\EMBRAPA\\ORTOMOSAICOS\\Sede_B3B4_1,5_190312\\3_dsm_ortho\\2_mosaic\\Sede_B3B4_1,5_190312_mosaic_group1.tif"
+
 file_path = select_file()
 print(f"Arquivo selecionado: {file_path}")
 
 
-# Verifica se a pasta "runs/segment/predict" existe, cria se não existir e limpa se existir
 runs_dir = './runs'
 if os.path.exists(runs_dir):
     shutil.rmtree(runs_dir)
 
-# Carrega a imagem
+output_indices_dir = './parcelas_indice'
+if os.path.exists(output_indices_dir):
+    shutil.rmtree(output_indices_dir)
+os.makedirs(output_indices_dir)
+
+
 image = cv2.imread(file_path)
 clone = image.copy()
 aoi_points = []
 
-# Função para desfazer o último ponto
 def undo_last_point():
     if aoi_points:
         aoi_points.pop()
 
-# Função para selecionar a AOI
 def select_aoi(image):
     fig, ax = plt.subplots()
-    
-    # Obtém a resolução da tela e ajusta o tamanho da janela
     monitor = get_monitors()[0]
     fig.set_size_inches((monitor.width*0.9) / fig.dpi, (monitor.height*0.7) / fig.dpi)
-    
     ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     ax.set_title("Clique para selecionar os pontos e delimitar a Área de Interesse.\nPressione 'u' para desfazer o último ponto.\n Pressione 'f' para finalizar.")
 
@@ -81,7 +73,7 @@ def select_aoi(image):
             fig.canvas.draw()
         elif event.key == 'f':
             if len(aoi_points) > 2:
-                aoi_points.append(aoi_points[0])  # Fecha o polígono
+                aoi_points.append(aoi_points[0])
                 fig.canvas.mpl_disconnect(cid)
                 fig.canvas.mpl_disconnect(kid)
                 plt.close(fig)
@@ -108,7 +100,6 @@ def show_cropped_image(image):
 
 
 def calculate_indices(red, green, blue, nir=None):
-    # Definir funções de cálculo para diferentes índices
     def bi(red, green, blue):
         return (2 * green - red - blue) / (2 * green + red + blue)
 
@@ -118,15 +109,12 @@ def calculate_indices(red, green, blue, nir=None):
     def ngrdi(red, green, nir):
         return (green - red) / (green + red - nir)
 
-    # Dicionário de funções de cálculo de índice
     index_functions = {
         "BI": bi,
         "GLI": gli,
         "NGRDI": lambda r, g, nir: ngrdi(r, g, nir)
-        # Adicione mais funções de índice conforme necessário
     }
 
-    # Calcular os índices selecionados
     index_results = {}
     for index_name, func in index_functions.items():
         if index_name == "BI" or index_name == "GLI":
@@ -136,116 +124,146 @@ def calculate_indices(red, green, blue, nir=None):
 
     return index_results
 
-
-# Função para processar cada imagem na pasta parcelas
 def process_parcelas(input_dir, output_dir):
-    # Listar todos os arquivos na pasta parcelas
     parcelas_files = os.listdir(input_dir)
-
-    # Iterar sobre cada imagem na pasta parcelas
     for filename in parcelas_files:
-        if filename.endswith(".png"):  # Apenas processa arquivos PNG (ajuste conforme necessário)
-            # Caminho completo para a imagem de entrada
+        if filename.endswith(".png"):
             input_path = os.path.join(input_dir, filename)
-
-            # Carregar a imagem
             image = cv2.imread(input_path)
-
-            # Dividir em canais RGB
             blue, green, red = cv2.split(image)
 
-            # # Salvar os canais Red, Green e Blue
-            # cv2.imwrite(os.path.join(output_dir, f"{os.path.splitext(filename)[0]}_red.png"), red)
-            # cv2.imwrite(os.path.join(output_dir, f"{os.path.splitext(filename)[0]}_green.png"), green)
-            # cv2.imwrite(os.path.join(output_dir, f"{os.path.splitext(filename)[0]}_blue.png"), blue)
-
             index_calculator = IndexCalculation(red=red, green=green, blue=blue)
-
             hue_index = index_calculator.ngrdi()
             cv2.imwrite("./hue.jpg", hue_index)
 
-            # Calcular os índices desejados
             indices = calculate_indices(red, green, blue)
 
-            # Salvar as imagens com os índices calculados na pasta parcelas_indice
             for index_name, result in indices.items():
-                # Criar pasta parcelas_indice se não existir
                 output_index_dir = os.path.join(output_dir, index_name)
                 os.makedirs(output_index_dir, exist_ok=True)
-
-                # Salvar imagem com o nome indicando o índice calculado
                 output_filename = f'{os.path.splitext(filename)[0]}_{index_name}.png'
                 output_path = os.path.join(output_index_dir, output_filename)
                 cv2.imwrite(output_path, image)
                 print(f"Imagem com índice '{index_name}' salva em: {output_path}")
 
+def rotate_image(image, angle):
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h))
+    return rotated
 
-# Chama a função para selecionar a AOI
+def get_rotation_angle():
+    root = Tk()
+    root.withdraw()
+    angle = simpledialog.askfloat("Input", "Digite o ângulo de rotação (em graus):")
+    root.destroy()
+    if angle is None:
+        print("Nenhum ângulo de rotação fornecido. Encerrando o programa.")
+        sys.exit(0)
+    return angle
+
+def expand_polygon(polygon, scale=1.1):
+    M = cv2.moments(polygon)
+    if M['m00'] == 0:
+        return polygon
+    cx = int(M['m10'] / M['m00'])
+    cy = int(M['m01'] / M['m00'])
+    center = np.array([cx, cy])
+    expanded_polygon = scale * (polygon - center) + center
+    return expanded_polygon.astype(np.int32)
+
+def find_top_left_polygon(polygons):
+    top_left = min(polygons, key=lambda p: (np.min(p[:, 1]), np.min(p[:, 0])))
+    return top_left
+
+def is_approximately_same_line(y1, y2, threshold=10):
+    return abs(y1 - y2) <= threshold
+
+
+def sort_polygons(polygons):
+    sorted_polygons = []
+    polygons = list(polygons)  # Converte para lista para manipulação
+
+    current_row = 0
+    while polygons:
+        top_left_polygon = find_top_left_polygon(polygons)
+        if top_left_polygon is None:
+            break
+        
+        top_left_index = polygons.index(top_left_polygon)
+        sorted_polygons.append((current_row, 0, top_left_polygon))
+        polygons.pop(top_left_index)
+
+        current_col = 1
+        y_reference = np.min(top_left_polygon[:, 1])
+
+        while True:
+            candidates = [p for p in polygons if is_approximately_same_line(np.min(p[:, 1]), y_reference)]
+            if not candidates:
+                break
+            
+            next_polygon = min(candidates, key=lambda p: np.min(p[:, 0]))
+            next_index = polygons.index(next_polygon)
+            sorted_polygons.append((current_row, current_col, next_polygon))
+            polygons.pop(next_index)
+            current_col += 1
+
+        current_row += 1
+
+        if polygons:
+            next_row_candidates = [p for p in polygons if np.min(p[:, 1]) > y_reference]
+            if next_row_candidates:
+                next_row_start = min(next_row_candidates, key=lambda p: np.min(p[:, 1]))
+                y_reference = np.min(next_row_start[:, 1])
+
+    return sorted_polygons
+
+
+
 select_aoi(clone)
 
 if len(aoi_points) < 3:
     raise ValueError("Área de interesse precisa ter ao menos 3 pontos")
 
-# Converte a lista de pontos para um array numpy
 aoi = np.array(aoi_points, np.int32)
 pts = aoi.reshape((-1, 1, 2))
 mask = np.zeros(clone.shape[:2], dtype=np.uint8)
 cv2.fillPoly(mask, [pts], 255)
-masked_image = cv2.bitwise_and(clone, clone, mask=mask)
+cropped_image = cv2.bitwise_and(clone, clone, mask=mask)
+
 x, y, w, h = cv2.boundingRect(pts)
-cropped_image = masked_image[y:y+h, x:x+w]
-image = cropped_image
-cv2.imwrite('img_cropped.png', image)
+cropped_image = cropped_image[y:y+h, x:x+w]
 
-show_cropped_image(image)
+angle = get_rotation_angle()
+rotated_image = rotate_image(cropped_image, angle)
+show_cropped_image(rotated_image)
 
-#
-#
-# Rotacionar crop, e permitir a rotação à 90°
-#
-#
+model_path = "best.pt"
+model = YOLO(model_path)
 
-# Carrega o modelo YOLO
-model = YOLO("best.pt")
+results = model(rotated_image)
 
-# Realiza a predição na imagem recortada
-results = model.predict(source=image, save=True, show_labels=False)
+output_dir = 'parcelas'
+os.makedirs(output_dir, exist_ok=True)
 
-# Verifica se a pasta "parcelas" existe, cria se não existir e limpa se existir
-output_dir = './parcelas'
-if os.path.exists(output_dir):
-    shutil.rmtree(output_dir)
-os.makedirs(output_dir)
-
-# Salva as parcelas detectadas
 cont = 0
-for result in results:
-    for poly in result.masks.xy:
-        poly = np.array(poly, dtype=np.int32)
-        mask = np.zeros_like(image)
+polygons = [np.array(poly, dtype=np.int32) for result in results for poly in result.masks.xy]
+sorted_polygons = sort_polygons(polygons)
 
-        cv2.fillPoly(mask, [poly], (255,) * mask.shape[2])
-        imagem_mascarada = cv2.bitwise_and(image, mask)
+for row, col, poly in sorted_polygons:
+    mask = np.zeros_like(rotated_image)
+    cv2.fillPoly(mask, [expand_polygon(poly, scale=1.1)], (255,) * mask.shape[2])
+    imagem_mascarada = cv2.bitwise_and(rotated_image, mask)
 
-        x, y, w, h = cv2.boundingRect(poly)
-        recorte = imagem_mascarada[y:y+h, x:x+w]
-        cv2.imwrite(f'{output_dir}/parcela-{cont}.png', recorte)
-        cont += 1
-        print(f'Salvando parcela {cont}')
+    x, y, w, h = cv2.boundingRect(expand_polygon(poly, scale=1.1))
+    recorte = imagem_mascarada[y:y+h, x:x+w]
 
+    cv2.imwrite(f'{output_dir}/parcela-{row}-{col}.png', recorte)
+    cont += 1
+    print(f'Salvando parcela {cont} na posição (linha {row}, coluna {col})')
 
-#
-#
-# Ordenar e nomear parcelas
-#
-#
-
-output_indices_dir = './parcelas_indice'
-if os.path.exists(output_indices_dir):
-        shutil.rmtree(output_indices_dir)
-os.makedirs(output_indices_dir)
 process_parcelas(output_dir, output_indices_dir)
-
 
 #
 #
